b200d61cf09da567e34996259e9e50456a115ab8
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-04-01 08:27:53 +0200

    Update update.ts

--- src/util/update.ts
+++ src/util/update.ts
@@ -52,13 +52,13 @@ export async function update(app: App, mainWindow: BrowserWindow): Promise<void>
                 }
                 if (process.platform == "darwin") {
 
-                    if (process.arch == "arm64") {
+         
 
-                        shell.openExternal(`https://github.com/GeekCornerGH/Threema-For-Desktop/releases/download/v${update[0].tag_name.replace("v", "")}/Threema-For-Desktop-mac-arm64-${update[0].tag_name.replace("v", "")}.dmg`);
+                        shell.openExternal(`https://github.com/GeekCornerGH/Threema-For-Desktop/releases/download/v${update[0].tag_name.replace("v", "")}/Threema-For-Desktop-mac-universal-${update[0].tag_name.replace("v", "")}.dmg`);
 
-                    } else {
+      
 
-                        shell.openExternal(`https://github.com/GeekCornerGH/Threema-For-Desktop/releases/download/v${update[0].tag_name.replace("v", "")}/Threema-For-Desktop-mac-x64-${update[0].tag_name.replace("v", "")}.dmg`);
+               
 
                     }
                     app.isQuiting = true;
